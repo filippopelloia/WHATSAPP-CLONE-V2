1ae4af2da955f1e6aa0cd1a0da9a551a34c61cbc
Dev1 - filip <pelloiafilippo@live.com>
2023-06-18 21:35:42 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Navbar from './Navbar.jsx'
 import {nanoid} from 'nanoid'
+import DataChat from './DataChat.jsx'
 
 function App() {
 
@@ -13,41 +14,65 @@ function App() {
 
   //I TUOI MESSAGGI
   const [text, setText] = useState('');
-  const [saveText, setSaveText] = useState([]);
+  // const [saveText, setSaveText] = useState([]);
 
   //I MESSAGGI DI RISPOSTA
-  const [textAnswer, setTextAnswer] = useState('Come va la vita?');
+  // const [textAnswer, setTextAnswer] = useState('Come va la vita?');
   // const [answer, setAnswer] = useState([]);
 
+  //CHAT
+  const [chat, setChat] = useState(DataChat);
 
+  // console.log(chat);
+  console.log(text);
 
 
   function saveInMemory(){
-    setSaveText(prevSaveText => [...prevSaveText, text]);
+    setChat(prevChat => [{...prevChat[counter], testo: text}, ...prevChat.slice(1)])
     setCounter(prevCounter => prevCounter + 1);
   }
 
 
+  console.log(chat);
 
+  // function saveInMemory(){
+  //   setSaveText(prevSaveText => [...prevSaveText, text]);
+  //   setCounter(prevCounter => prevCounter + 1);
+  // }
 
-  const showMessage = saveText?.map(item => {
-    return <div className='section right-side'>
-              <div key={nanoid()} className='message'>{item}</div>
-           </div>
+  const showChat = chat.map(item => {
+    return <>
+              <div className='section right-side'>
+                <div key={nanoid()} className='message'>{item.testo}</div>
+              </div>
+
+              <div className='section'>
+                <div key={nanoid()} className='message'>{item.answer}</div>
+              </div>
+           </>
   })
 
-  let risposta;
-
-  if(counter >= 1){
-  risposta =  (<div className='section'>
-                        <div key={nanoid()} className='message'>{textAnswer}</div>
-                    </div>)
-  } else {
-    console.log('Nada');
-  }
 
 
-  console.log(counter);
+
+  // const showMessage = saveText?.map(item => {
+  //   return <div className='section right-side'>
+  //             <div key={nanoid()} className='message'>{item}</div>
+  //          </div>
+  // })
+
+  // let risposta;
+
+  // if(counter >= 1){
+  // risposta =  (<div className='section'>
+  //                       <div key={nanoid()} className='message'>{textAnswer}</div>
+  //                   </div>)
+  // } else {
+  //   console.log('Nada');
+  // }
+
+
+  // console.log(counter);
 
 
   return (
@@ -60,9 +85,15 @@ function App() {
               <div className='message'>Ciao amico miooo!!</div>
             </div>
 
-            {showMessage}
+            {/* <h3>{chat[0].answer}</h3> */}
 
-            {risposta}
+            {showChat}
+
+{/* 
+            {showMessage}
+            
+
+            {risposta} */}
 
             {/* <div className='section'>
               <div className='message'>Hey Filippo!!</div>
---
+++ src/DataChat.jsx
@@ -0,0 +1,7 @@
+const DataChat = [
+    {id: 0, testo: '', timeText: '', answer: 'Heyyyy!!', timeAnswer: '18:11'},
+    {id: 1, testo: '', timeText: '', answer: 'Come stai Filippo??', timeAnswer: '18:19'},
+    {id: 2, testo: '', timeText: '', answer: 'Io sto alla grande qui in Portogallo!!!', timeAnswer: '18:32'},
+]
+
+export default DataChat;
